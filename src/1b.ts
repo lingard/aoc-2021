9ccda1c3f129fcf45297fc77205f63c1c2b7040c
Christer Lingard <lingard.christer@gmail.com>
2021-12-02 14:31:10 +0100

    use readInput in 1b

--- src/1b.ts
+++ src/1b.ts
@@ -1,17 +1,15 @@
 import { constant, pipe } from 'fp-ts/lib/function'
 import * as RA from 'fp-ts/ReadonlyArray'
 import * as TE from 'fp-ts/TaskEither'
-import * as E from 'fp-ts/Either'
 import * as M from 'fp-ts/Monoid'
 import * as n from 'fp-ts/number'
 import * as b from 'fp-ts/boolean'
 
-import { AppError, readFile } from './util'
-import { increases, Input } from './1a'
+import { increases, Input, readInput } from './1a'
 
-const chopN = (n: number) => (i: Input) =>
+const chopN = (n: number) => (ns: ReadonlyArray<number>) =>
   pipe(
-    i,
+    ns,
     RA.chop((as) => [
       pipe(as, RA.takeLeft(n)),
       pipe(
@@ -21,13 +19,8 @@ const chopN = (n: number) => (i: Input) =>
     ])
   )
 
-const parseInput = (input: string) =>
-  pipe(input.split('\n'), Input.decode, E.mapLeft(AppError.ValidationErrors))
-
 export const main = pipe(
-  readFile('input/1'),
-  TE.mapLeft(AppError.FSError),
-  TE.chainEitherK(parseInput),
+  readInput('input/1', Input),
   TE.map((values) =>
     pipe(values, chopN(3), RA.map(M.concatAll(n.MonoidSum)), increases)
   )
